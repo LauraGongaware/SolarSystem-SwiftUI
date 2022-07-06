334ed57ec51e64b6004700f836b8623cdedf80bb
Laura Gongaware <LauraGongaware@gmail.com>
2022-07-06 13:25:01 -0700

    Initial commit

---
+++ SolarSystem/SolarSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemApp.swift
+//  SolarSystem
+//
+//  Created by Laura Gongaware on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
